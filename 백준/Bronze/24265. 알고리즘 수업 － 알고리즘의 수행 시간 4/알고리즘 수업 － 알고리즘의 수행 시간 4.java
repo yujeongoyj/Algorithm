84b2ad17efb2549410e98eaf126462cc8a5ca0ea
yujeongoyj <120093994+yujeongoyj@users.noreply.github.com>
2024-12-01 21:48:42 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 120 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write((long)n*(n-1)/2 + "\n" + 2);
+        bw.flush();
+
+
+    }
+}
